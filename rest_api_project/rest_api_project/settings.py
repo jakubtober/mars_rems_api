6442402ea2137a457bb7aeb0d1e9d8267c28a765
Jakub Tober <jakub.tober@gmail.com>
2018-10-21 15:23:45 +0200

    More changes to settings.py

--- rest_api_project/rest_api_project/settings.py
+++ rest_api_project/rest_api_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8dx*7o$ulywla*6gr^vq&3a$-k8lsv_zr)_wd-u&bop2%qjn9d'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
